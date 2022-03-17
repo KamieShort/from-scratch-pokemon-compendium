1d2b5fa307f44e2b6d196599d9e7ed5631354371
Kamie Short <kamie.short@gmail.com>
2022-03-17 15:22:41 -0700

    search bar functioning correctly

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-export default function Search({ query, setQuery }) {
+export default function Search({ setQuery }) {
   const [input, setInput] = useState('');
 
   return (
     <div>
-      <input onChange={(e) => setInput(e.target.value)} placeholder="See this placeholder" />
+      <input onChange={(e) => setInput(e.target.value)} placeholder="Search" />
       <button onClick={() => setQuery(input)}>Search</button>
     </div>
   );
--- src/services/fetchpokemon.js
+++ src/services/fetchpokemon.js
@@ -12,7 +12,7 @@ export async function fetchByType() {
   return data.map((item) => item.type);
 }
 
-export async function filterPokemon(type) {
+export async function filterPokemon(type, search) {
   const params = new URLSearchParams();
 
   params.set('perPage', 10);
@@ -20,6 +20,11 @@ export async function filterPokemon(type) {
   if (type !== 'all') {
     params.set('type', type);
   }
+
+  if (search) {
+    params.set('pokemon', search);
+  }
+
   const resp = await fetch(
     `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
   );
